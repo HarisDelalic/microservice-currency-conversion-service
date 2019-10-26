a340caa7bca0b8f05c1942a73dc99d32fba35dc5
delalic <delalicharis@gmail.com>
2019-10-27 01:43:15 +0200

    use eureka dependency, microservice becomes eureka client

--- src/main/java/com/dela/microservicecurrencyconversionservice/MicroserviceCurrencyConversionServiceApplication.java
+++ src/main/java/com/dela/microservicecurrencyconversionservice/MicroserviceCurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.dela.microservicecurrencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients("com.dela.microservicecurrencyconversionservice.configs.feign")
 public class MicroserviceCurrencyConversionServiceApplication {
 
